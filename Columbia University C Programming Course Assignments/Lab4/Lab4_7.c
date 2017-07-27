e827f9a2c38655e1c295faa6d33fbe22c23dc031
AntonioDaSilva <mertunsal1905@gmail.com>
2017-07-27 19:27:16 +0300

    Add files via upload

--- Columbia University C Programming Course Assignments/Lab4/Lab4_7.c
+++ Columbia University C Programming Course Assignments/Lab4/Lab4_7.c
@@ -8,8 +8,45 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
+  int arr1[20];
+  int arr2[20];
+  int i = 0;
+  do{
+    printf("Array 1 element %d (enter -1 to exit): ",i+1);
+    scanf("%d",&arr1[i]);
+    i++;
+  } while (arr1[i - 1] != -1 && i < 20);
 
+  for (int k = i-1; k<20; k++){
+    arr1[k] = 0;
+  }
+
+  i = 0;
+  do{
+    printf("Array 2 element %d (enter -1 to exit): ",i+1);
+    scanf("%d",&arr2[i]);
+    i++;
+  } while (arr2[i - 1] != -1  && i < 20);
+
+  for (int k = i-1; k<20; k++){
+    arr2[k] = 0;
+  }
+
+  int arr3[20];
+
+  double sum = 0;
+    
+  for (int j = 0; j<20; j++){
+    arr3[j] = arr1[j] * arr2[j];
+    sum += arr3[j]*arr3[j];
+    printf("%d   *    %d    =    %d\n",arr1[j], arr2[j], arr3[j]);
+  }
+
+  sum = sqrt(sum);
+
+  printf("Sum of squareroots: %lf\n",sum);
   return 0;
 }
