629eee01e3fc2ad1df1f59c432ae003265ab664e
AntonioDaSilva <mertunsal1905@gmail.com>
2017-07-27 19:49:09 +0300

    Update Lab5_7.c

--- Columbia University C Programming Course Assignments/Lab5/Lab5_7.c
+++ Columbia University C Programming Course Assignments/Lab5/Lab5_7.c
@@ -8,8 +8,45 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+
+int lengthof(char arr[]){
+  int count = 0;
+  for (int i = 0; i<MAXLEN; i++){
+    if (arr[i] != '\0'){
+      count += 1;
+    }
+    else {
+      break;
+    }
+  }
+  return count;
+}
 
 int main(){
-
-  return 0;
+  char str1[100];
+  char str2[100];
+  printf("First string: ");
+  scanf("%s",str1);
+  printf("Second string: ");
+  scanf("%s",str2);
+    
+  int len1 = lengthof(str1);
+  int len2 = lengthof(str2);
+  
+  char str3[100];
+  
+  for (int i = 0; i < (len1 > len2 ? len2 : len1); i++){
+        if (str1[len1 - i] == str2[len2 - i]){
+          str3 = str1[len1-i];
+        }
+        else {
+          break;
+        }
+  }
+  
+  char str_reversed = strrev(str3);
+  printf("%s\n",str_reversed);
+  
+  return 0;    
 }
